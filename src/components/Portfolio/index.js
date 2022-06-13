9a05d95bb0d8a337eec62e15547081ddbbbc3fd5
= <holdanmcmullen@gmail.com>
2022-06-12 20:55:27 -0600

    added hover on apps

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,18 +10,22 @@ function Portfoilo() {
             <h2 className="section-title">Portfolio</h2>
             <div className="apps-display">
                 {apps.map((app) => (
-                    <div className="card" key={app.title} title={app.title}>
+                    <article className="card" key={app.title} title={app.title}>
                         <div className="card-title">
                             <h4>{app.title}</h4>
                             <p>{app.text}</p>
                         </div>
-                        <a href={app.deployLink} target="_blank">
-                            <img
+                        <img
+                            className='cardImg'
                             src={require(`../../assets/images/${app.image}`)}
                             alt={app.alt}
-                            />
-                        </a>
-                    </div>
+                        />
+                        <div className="middleContent">
+                            <a href={app.deployLink} target="_blank">Deployed Link</a>
+                            <br /><br />
+                            <a href={app.githubLink} target="_blank">Github Repo</a>
+                        </div>
+                    </article>
                 ))}
             </div>
         </section>
