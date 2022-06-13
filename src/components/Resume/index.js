a6f7ddfd914ef025de98052b35e6ac013c5cccd8
= <holdanmcmullen@gmail.com>
2022-06-12 19:31:19 -0600

    fixed Linkedin link

---
+++ src/components/Resume/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resume() {
+    return (
+        <section id="resume" className="resume">
+            <h2 className="section-title">My Resume</h2>
+        </section>
+    )
+}
+
+export default Resume;
